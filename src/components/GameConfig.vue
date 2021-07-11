a00ac1071ffb7a8deed7ee9a9b1dcb35133e7e77
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2021-07-10 23:59:58 -0400

    Started pages

---
+++ src/components/GameConfig.vue
@@ -0,0 +1,124 @@
+<template>
+    
+    <main>
+        <label for="">Number of Cards</label>
+        <section class="cardamount">
+            <button v-for="(item, index) in cardAmount" :class="[item.selected ? 'selected' : 'unselected']" v-on:click="selectAmount(index)">{{item.amount}} Cards</button>
+        </section>
+        <div style="height: 50px"></div>
+        <label for="" class="time-label">Time Given</label>
+        <section class="timeoption">
+            <button v-for="(item, index) in timings" :class="[item.selected ? 'selected' : 'unselected']" v-on:click="selectTime(index)">{{item.seconds}} Seconds</button>
+        </section>
+        <button class="startbutton">Start Game!</button>
+    </main>
+</template>
+
+<script>
+
+
+export default {
+    name: "GameConfig",
+    data(){
+        return{
+            cardAmount:[
+                {amount: 5, selected: true},
+                {amount: 8, selected: false},
+                {amount: 10, selected: false},
+                {amount: 13, selected: false},
+                {amount: 15, selected: false},
+                {amount: 20, selected: false}
+
+            ],
+            timings:[
+                {seconds: 5, selected: false},
+                {seconds: 10, selected: true},
+                {seconds: 15, selected: false},
+                {seconds: 20, selected: false},
+                {seconds: 25, selected: false}
+            ]
+        }
+    },
+    methods:{
+        selectTime(index){
+            console.log(this.timings[index].selected);
+            for (let i = 0; i < this.timings.length;i++){
+                if(i != index){
+                    this.timings[i].selected = false;
+                }
+                else{
+                    this.timings[i].selected = true;
+                }
+            }
+        },
+        selectAmount(index){
+            for(let i = 0; i < this.cardAmount.length; i++){
+                if(i != index){
+                    this.cardAmount[i].selected = false;
+                }
+                else{
+                    this.cardAmount[i].selected = true;
+                }
+            }
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+	@import "../assets/styles.scss";
+
+    @mixin button-options{
+        flex: 1;
+        padding: 10px 0px;
+        margin: 0px 5px;
+        font-size: 1.5rem;
+        background: none;
+
+    }
+
+    main{
+        background-color: beige;
+        width: 75%;
+        height: 500px;
+        margin: auto;
+        margin-top: 50px;
+
+        & > section{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-evenly;
+            margin: auto;
+
+            & > button{
+                @include button-options;
+            }
+        }
+    }
+    
+    label{
+        font-size: 2rem;
+        margin: auto;
+    }
+
+
+
+    .selected{
+        border: solid 2px $primary-colour;
+        color: $primary-colour;
+    }
+    .unselected{
+        border: solid 2px $black;
+        color: $black;
+    }
+
+    .timeoption{
+
+    }
+    .cardamount{
+        flex-wrap: wrap;
+    }
+
+    .startbutton{
+        
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,118 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
+  	<button v-on:click="guestPlay" v-if="!front" class="back-button">Back to Home</button>
+	<GameCongif v-if="!front"/>
+	<main v-if="front">
+		<h1>POKER MEMORY</h1>
+		<p>A game which help you train your memory and thinking</p>
+		<section>
+			<button>Play</button>
+			<button v-on:click="guestPlay">Play as Guest</button>
+		</section>
+	</main>
+	<section class="background"></section>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import GameCongif from "@/components/GameConfig.vue"
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	data(){
+		return{
+			front: true,
+		}
+	},
+	components: {
+	HelloWorld,
+	GameCongif
+	},
+	methods:{
+		guestPlay(){
+			if(this.front){
+				this.front = false;
+			}
+			else{
+				this.front = true;
+			}
+		}
+	}
 }
 </script>
+
+<style lang="scss" scoped>
+	@import "../assets/styles.scss";
+	h1{
+		font-size: 5rem;
+		margin: auto;
+		color: $primary-colour;
+	}
+	main{
+
+		position: absolute;
+		z-index: 0;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -70%);
+		width: 60%;
+
+		& > section{
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			width: 60%;
+			margin: auto;
+			margin-top: 50px;
+
+
+			& > button{
+				width: 200px;
+				font-size: 1.5rem;
+				padding: 10px 0px;
+				background-color: $primary-colour;
+				color: white;
+
+				&:hover{
+					background-color: $primary-hover;
+				}
+
+			}
+		}
+	}
+	p{
+		color: $sub-black;
+		font-size: 1.5rem;
+		width: 75%;
+		margin: auto;
+
+	}
+	.background{
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		z-index: -2;
+		background-color: $bgcolour;
+	}
+
+	.back-button{
+		position: absolute;
+		border: solid 2px $primary-colour;
+		color: $primary-colour;
+		font-size: 1.5rem;
+		width: 200px;
+		padding: 10px 0px;
+		top: 20px;
+		left: 40px;
+		background-color: white;
+
+		&:hover{
+			color: $primary-hover;
+			border: solid 2px $primary-hover;
+		}
+	}
+</style>
