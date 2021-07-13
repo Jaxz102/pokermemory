624c9874ce82c87d6ee432d131313ec9598f5c7f
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2021-07-13 18:11:11 -0400

    Did pictures start

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,6 +1,9 @@
 <template>
     <h1>{{message}}</h1>
-    <main></main>
+    <img src="../assets/cards/9H.png" alt="">
+    <main>
+        <img v-for="(item, index) in cards" v-bind:src="'../assets/cards/' + item" />
+    </main>
 </template>
 <script>
 
@@ -9,16 +12,39 @@ export default {
     name: "Game",
     data(){
         return{
-            message: "Remember your cards!"
+            suite:["D", "C", "H", "S"],
+            message: "Remember your cards!",
+            cards:[]
         }
+    },
+    mounted(){
+        let amount = this.$store.state.cardAmount;
+        let previous = 0;
+        for(let i = 0; i < amount; i++){
+            let picknum = Math.floor(Math.random()*13) + 1;
+            let picksuite = Math.floor(Math.random()*4);
+            while (picknum == previous){
+                picknum = Math.floor(Math.random()*52) + 1;
+            }
+            previous = picknum;
+            this.cards.push(`${picknum}${this.suite[picksuite]}.png`)
+        }
+        previous = 0;
     }
 }
 </script>
 
 <style lang="scss" scoped>
+    @import "../assets/styles.scss";   
     main{
         width: 75%;
         height: 50%;
         background-color: aliceblue;
+        
+    }
+
+    img{
+        width: 200px;
+        height: 400px;
     }
 </style>
